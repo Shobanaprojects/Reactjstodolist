63547400bbae07eed35af283f3734da3b7472f72
Shobana <shobzzz125@gmail.com>
2024-10-13 13:04:34 +0530

    add updated files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AddTodo from './components/AddTodo';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Fragment>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AddTodo />} />
+        </Routes>
+      </BrowserRouter>
+    </Fragment>
+  )
 }
 
 export default App;
